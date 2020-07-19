c8ed114df843962fdab230ff0739a170198ec6f7
tomE177 <46957425+tomE177@users.noreply.github.com>
2020-07-19 18:04:12 +1000

    Contained Vector3 & Vector4
    All unit tests involving only Vector3 & Vector4 have been completed

--- C# Unit Test - Student Copy/MathClasses/Vector3.cs
+++ C# Unit Test - Student Copy/MathClasses/Vector3.cs
@@ -8,7 +8,7 @@ namespace MathClasses
 {
     public class Vector3
     {
-        //public float x, y, z;
+        public float x, y, z;
         
 
         public float[] xyz = new float[3];
@@ -19,6 +19,10 @@ namespace MathClasses
             xyz[0] = 0;
             xyz[1] = 0;
             xyz[2] = 0;
+
+            x = 0;
+            y = 0;
+            z = 0;
         }
 
         public Vector3(float X, float Y, float Z)
@@ -26,8 +30,44 @@ namespace MathClasses
             xyz[0] = X;
             xyz[1] = Y;
             xyz[2] = Z;
+
+            x = X;
+            y = Y;
+            z = Z;
         }
 
+        //functions
+        void UpdatePoints()
+        {
+            x = xyz[0];
+            y = xyz[1];
+            z = xyz[2];
+        }
+
+        public float Dot(Vector3 v3)
+        {
+            return x * v3.x + y * v3.y + z * v3.z;
+        }
+
+        public Vector3 Cross(Vector3 v3)
+        {
+            return new Vector3((y * v3.z - z * v3.y), (z * v3.x - x * v3.z), (x * v3.y - y * v3.x));
+        }
+
+        public float Magnitude()
+        {
+            return (float)Math.Sqrt((x*x) + (y*y) + (z*z));
+        }
+
+        public void Normalize()
+        {
+            Vector3 vector = this / Magnitude();
+            xyz = vector.xyz;
+            UpdatePoints();
+        }
+
+
+        //overloaders
         public static Vector3 operator *(Matrix3 lhs, Vector3 rhs)
         {
             Vector3 newVector3 = new Vector3();
@@ -42,6 +82,68 @@ namespace MathClasses
 
             return newVector3;
         }
+
+        public static Vector3 operator +(Vector3 v1, Vector3 v2)
+        {
+            Vector3 v3 = new Vector3();
+
+            for(int y = 0; y < 3; y++)
+            {
+                v3.xyz[y] = v1.xyz[y] + v2.xyz[y];
+            }
+            v3.UpdatePoints();
+            return v3;
+        }
+
+        public static Vector3 operator -(Vector3 v1, Vector3 v2)
+        {
+            Vector3 v3 = new Vector3();
+
+            for (int y = 0; y < 3; y++)
+            {
+                v3.xyz[y] = v1.xyz[y] - v2.xyz[y];
+            }
+            v3.UpdatePoints();
+            return v3;
+        }
+
+
+        public static Vector3  operator *(Vector3 v1, float scaler)
+        {
+
+            for (int i = 0; i < 3; i++)
+            {
+                v1.xyz[i] *= scaler;
+            }
+            v1.UpdatePoints();
+            return v1;
+        }
+
+        public static Vector3 operator *(float scaler, Vector3 v1)
+        {
+            return v1 * scaler;
+        }
+
+
+        public static Vector3 operator /(Vector3 v1, float scaler)
+        {
+
+            for (int i = 0; i < 3; i++)
+            {
+                v1.xyz[i] = v1.xyz[i]/scaler;
+            }
+            v1.UpdatePoints();
+            return v1;
+        }
+
+        public static Vector3 operator /(float scaler, Vector3 v1)
+        {
+            return v1 * scaler;
+        }
+
+
+
+
 
     }
 }
--- C# Unit Test - Student Copy/MathClasses/Vector4.cs
+++ C# Unit Test - Student Copy/MathClasses/Vector4.cs
@@ -10,12 +10,134 @@ namespace MathClasses
     {
         public float x, y, z, w;
 
+        public float[] xyzw = new float[4];
+
         public Vector4()
         {
+            xyzw[0] = 0;
+            xyzw[1] = 0;
+            xyzw[2] = 0;
+            xyzw[3] = 0;
+
             x = 0;
             y = 0;
             z = 0;
             w = 0;
+        }
+
+        public Vector4(float X, float Y, float Z, float W)
+        {
+            xyzw[0] = X;
+            xyzw[1] = Y;
+            xyzw[2] = Z;
+            xyzw[3] = W;
+
+            x = X;
+            y = Y;
+            z = Z;
+            w = W;
+        }
+
+        //Functions
+        void UpdatePoints()
+        {
+            x = xyzw[0];
+            y = xyzw[1];
+            z = xyzw[2];
+            w = xyzw[3];
+        }
+
+        public float Dot(Vector4 v4)
+        {
+            return x * v4.x + y * v4.y + z * v4.z + w * v4.w;
+        }
+
+        public Vector4 Cross(Vector4 v3)
+        {
+            return new Vector4((y * v3.z - z * v3.y), (z * v3.x - x * v3.z), (x * v3.y - y * v3.x),0);
+        }
+
+        public float Magnitude()
+        {
+            return (float)Math.Sqrt((x * x) + (y * y) + (z * z) + (w*w));
+        }
+
+        public void Normalize()
+        {
+            Vector4 vector = this / Magnitude();
+            xyzw = vector.xyzw;
+            UpdatePoints();
+        }
+
+        //Overloads
+        public static Vector4 operator +(Vector4 v1, Vector4 v2)
+        {
+            Vector4 v3 = new Vector4();
+
+            for (int y = 0; y < 4; y++)
+            {
+                v3.xyzw[y] = v1.xyzw[y] + v2.xyzw[y];
+            }
+            v3.UpdatePoints();
+            return v3;
+        }
+
+        public static Vector4 operator -(Vector4 v1, Vector4 v2)
+        {
+            Vector4 v3 = new Vector4();
+
+            for (int y = 0; y < 4; y++)
+            {
+                v3.xyzw[y] = v1.xyzw[y] - v2.xyzw[y];
+            }
+            v3.UpdatePoints();
+            return v3;
+        }
+
+        public static Vector4 operator *(Vector4 v1, float scaler)
+        {
+
+            for (int i = 0; i < 4; i++)
+            {
+                v1.xyzw[i] *= scaler;
+            }
+            v1.UpdatePoints();
+            return v1;
+        }
+
+        public static Vector4 operator *(float scaler, Vector4 v1)
+        {
+            return v1 * scaler;
+        }
+
+        public static Vector4 operator /(Vector4 v1, float scaler)
+        {
+
+            for (int i = 0; i < 4; i++)
+            {
+                v1.xyzw[i] = v1.xyzw[i]/scaler;
+            }
+            v1.UpdatePoints();
+            return v1;
+        }
+
+        public static Vector4 operator /(float scaler, Vector4 v1)
+        {
+            return v1 * scaler;
+        }
+
+
+        //treat as vector4 = vector3
+        public static Vector4 operator ^(Vector4 v1, Vector3 v3)
+        {
+
+            for(int i = 0;i < 3; i++)
+            {
+                v1.xyzw[i] = v3.xyz[i];
+            }
+            v1.xyzw[3] = 0;
+            v1.UpdatePoints();
+            return v1;
         }
 
     }
