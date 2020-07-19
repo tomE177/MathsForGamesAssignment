8b0b701ee285e418066ed4b2e96da439ca0bef9f
tomE177 <46957425+tomE177@users.noreply.github.com>
2020-07-19 16:16:19 +1000

    Real test unit project
    pushed the wrong project last push, this is the proper one

--- C# Unit Test - Student Copy/MathClasses/Matrix3.cs
+++ C# Unit Test - Student Copy/MathClasses/Matrix3.cs
@@ -8,13 +8,59 @@ namespace MathClasses
 {
     public class Matrix3
     {
-        public float m1, m2, m3, m4, m5, m6, m7, m8, m9;
+
+        //public Matrix3()
+        //{
+        //    m1 = 1; m2 = 0; m3 = 0;
+        //    m4 = 0; m5 = 1; m6 = 0;
+        //    m7 = 0; m8 = 0; m9 = 1;
+        //}
+        public Vector3[] Axis = new Vector3[3]; //xyz
+
+        public Matrix3(Vector3 XAxis, Vector3 YAxis, Vector3 ZAxis)
+        {
+            Axis[0] = XAxis;
+            Axis[1] = YAxis;
+            Axis[2] = ZAxis;
+        }
 
         public Matrix3()
         {
-            m1 = 1; m2 = 0; m3 = 0;
-            m4 = 0; m5 = 1; m6 = 0;
-            m7 = 0; m8 = 0; m9 = 1;
+            for (int i = 0; i < 3; ++i)
+            {
+                Axis[i] = new Vector3();
+            }
+
+            Axis[0].xyz[0] = 1;
+            Axis[0].xyz[1] = 0;
+            Axis[0].xyz[2] = 0;
+
+            Axis[1].xyz[0] = 0;
+            Axis[1].xyz[1] = 1;
+            Axis[1].xyz[2] = 0;
+
+            Axis[2].xyz[0] = 0;
+            Axis[2].xyz[1] = 0;
+            Axis[2].xyz[2] = 1;
+        }
+
+        public static Matrix3 operator *(Matrix3 lhs, Matrix3 rhs)
+        {
+            Matrix3 multiMatrix = new Matrix3();
+
+            for (int i = 0; i < 3; ++i)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    for (int x = 0; x < 3; x++)
+                    {
+                        multiMatrix.Axis[y].xyz[i] = multiMatrix.Axis[y].xyz[i] + rhs.Axis[i].xyz[x] * lhs.Axis[x].xyz[y];
+                    }
+                }
+            }
+
+
+            return multiMatrix;
         }
 
     }
--- C# Unit Test - Student Copy/MathClasses/Vector3.cs
+++ C# Unit Test - Student Copy/MathClasses/Vector3.cs
@@ -8,13 +8,39 @@ namespace MathClasses
 {
     public class Vector3
     {
-        public float x, y, z;
+        //public float x, y, z;
+        
+
+        public float[] xyz = new float[3];
+
 
         public Vector3()
         {
-            x = 0;
-            y = 0;
-            z = 0;
+            xyz[0] = 0;
+            xyz[1] = 0;
+            xyz[2] = 0;
+        }
+
+        public Vector3(float X, float Y, float Z)
+        {
+            xyz[0] = X;
+            xyz[1] = Y;
+            xyz[2] = Z;
+        }
+
+        public static Vector3 operator *(Matrix3 lhs, Vector3 rhs)
+        {
+            Vector3 newVector3 = new Vector3();
+
+            for (int y = 0; y < 3; y++)
+            {
+                for (int x = 0; x < 3; ++x)
+                {
+                    newVector3.xyz[y] = newVector3.xyz[y] + lhs.Axis[x].xyz[y] * rhs.xyz[x];
+                }
+            }
+
+            return newVector3;
         }
 
     }
